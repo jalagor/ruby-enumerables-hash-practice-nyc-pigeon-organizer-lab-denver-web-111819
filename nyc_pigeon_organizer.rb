573fd63fb2f336f5e60248f8d55811218c710863
John Rogala <johnrossrogala@gmail.com>
2019-11-15 14:24:33 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,5 +2,9 @@ def nyc_pigeon_organizer(data)
   pigeons_by_names = {}
   data.each do |key, value|
     value.each do |key1, value1|
-      value1.each do 
+      value1.each do |name|
+        if pigeons_by_names[name] == nil 
+          pigeons_by_names[name] = {}
+        end
+        
 end
